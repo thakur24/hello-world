98457273c0cd846412796574e11dd63271bf4630
Javvaji Sumanth <46456270+JavvajiSumanth@users.noreply.github.com>
2019-10-10 22:11:43 +0530

    C Program to create a full pyramid pattern #10
     Pattern Printing Program to create a Full Pyramid Pattern

---
+++ FullPyramid.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+#define MAX	5 // setting variable MAX to 5
+
+int main()
+{
+	int i,j; // declaration of variables
+	int space=4;
+	/*run loop (parent loop) till number of rows*/
+	for(i=0;i< MAX;i++)
+	{
+		/*loop for initially space, before star printing*/
+		for(j=0;j< space;j++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<=i;j++)
+		{
+			printf("* ");
+		}
+		
+		printf("\n");
+		space--;
+	}
+	/*repeat it again*/
+	space=0;
+	/*run loop (parent loop) till number of rows*/
+	for(i=MAX;i>0;i--)
+	{
+		/*loop for initially space, before star printing*/
+		for(j=0;j< space;j++)
+		{
+			printf(" ");
+		}
+		for(j=0;j< i;j++)
+		{
+			printf("* ");
+		}
+		
+		printf("\n");
+		space++;
+	}
+    return 0;
+}
+
+/* Output Example
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+*/
